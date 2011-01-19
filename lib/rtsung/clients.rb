eb43f54eb7a469c9f8178be5c79b6f115c1007c7
Tema Ivanov <noxyu3m@gmail.com>
2011-01-19 21:32:14 +0300

    clients refactoring and options added

--- lib/rtsung/clients.rb
+++ lib/rtsung/clients.rb
@@ -2,8 +2,27 @@ class RTsung
   class Clients < Base
     private
 
-    def client(host)
-      xml.client(:host => host)
+    # running with a job scheduler is not supported
+    def host(name, options = {}, &block)
+      attrs = {
+        :host => name
+      }
+
+      [:use_controller_vm, :weight, :maxusers, :cpu].each do |o|
+        attrs[o] = options[o] if options[o]
+      end
+
+      if block_given?
+        xml.client(attrs) do
+          instance_eval(&block)
+        end
+      else
+        xml.client(attrs)
+      end
+    end
+
+    def ip(address)
+      xml.ip(:value => address)
     end
   end
 end
